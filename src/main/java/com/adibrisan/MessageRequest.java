b0f4291325879e1fd3e54d66c15f84c9027a338a
adibrisan <adi.brisan@yahoo.com>
2025-07-12 21:50:43 +0300

    first commit

---
+++ src/main/java/com/adibrisan/MessageRequest.java
@@ -0,0 +1,4 @@
+package com.adibrisan;
+
+public record MessageRequest(String message) {
+}
---
+++ src/test/java/com/adibrisan/KafkaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adibrisan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
